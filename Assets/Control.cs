de821189663543f5e190d4af9b859d6b3afc54c8
lanpai <lanpai@piyo.cafe>
2019-04-02 21:51:15 -0700

    Init

---
+++ Assets/Control.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+    public Rigidbody rb;
+    public GameObject projectile;
+    public Transform projLocation;
+    public ParticleSystem muzzleFlash;
+
+    private float nextFireTimestamp;
+
+    public float FIRE_ANGLE;
+    public float SHOT_COUNT;
+    public float RELOAD_COOLDOWN = 2f;
+    public float KICKBACK_FORCE = 100000f;
+    public float TORQUE = 300000f;
+
+    public string KEY_FIRE;
+    public string KEY_CCW;
+    public string KEY_CW;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - nextFireTimestamp >= 0) {
+
+            if (Input.GetKeyDown(KEY_FIRE)) {
+                muzzleFlash.Emit(10);
+                nextFireTimestamp = Time.time + RELOAD_COOLDOWN;
+
+                // APPLY BACKWARDS FORCE
+                rb.AddForce(transform.right * -1 * KICKBACK_FORCE);
+
+                // FIRE PROJECTILE AND PARTICLES
+                for (int i = 0; i < SHOT_COUNT; i++) {
+                    GameObject bullet = Instantiate(projectile, projLocation.position, Quaternion.identity);
+                    bullet.SetActive(true);
+                    Rigidbody bulletRb = bullet.GetComponent(typeof(Rigidbody)) as Rigidbody;
+                    bulletRb.velocity =
+                        Quaternion.AngleAxis(
+                            Random.Range(-1 * FIRE_ANGLE, FIRE_ANGLE),
+                            Vector3.forward)
+                        * transform.right * 800f;
+                }
+            }
+        }
+
+        bool ccw = Input.GetKeyDown(KEY_CCW);
+        bool cw = Input.GetKeyDown(KEY_CW);
+        float torque = 0;
+        if (!(ccw && cw) && (ccw || cw)) {
+            torque = TORQUE;
+            if (cw) {
+                // TURN CLOCKWISE
+                torque *= -1;
+            }
+        }
+        rb.AddTorque(transform.forward * torque);
+    }
+}
---
+++ Assets/ProjectileCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileCollision : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    private float spawnTimestamp;
+    private Vector3 prevVel;
+    private int reflectCount = 0;
+
+    private const int REFLECT_MAX = 2;
+    private const float LIFETIME_MAX = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnTimestamp = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - spawnTimestamp > LIFETIME_MAX)
+            Destroy(gameObject);
+    }
+
+    void FixedUpdate() {
+        prevVel = rb.velocity;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        ContactPoint contact = collision.contacts[0];
+        Vector3 reflectedVel = Vector3.Reflect(prevVel, contact.normal);
+        rb.velocity = reflectedVel;
+        
+        reflectCount++;
+        if (reflectCount >= REFLECT_MAX) Destroy(gameObject);
+    }
+}
